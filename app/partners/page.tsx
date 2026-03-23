216302eadbe8712f6fe27ece94040f53d00fae7a
PlayableFitness <hello@playable-fitness.com>
2026-03-23 22:49:34 +0100

    Update page.tsx

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -159,7 +159,7 @@ export default function PartnersPage() {
               Für Partner
             </p>
 
-            <h1 className="text-5xl font-semibold leading-[0.98] tracking-tight md:text-7xl xl:text-[6.5rem]">
+            <h1 className="text-4xl font-semibold leading-[0.98] tracking-tight md:text-7xl xl:text-[6.5rem]">
               Werde Teil von
               <br />
               Bewegungskultur.
